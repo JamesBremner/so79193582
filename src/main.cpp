3dd1a55b199797a1bc410adf7d32f1cada5f0bb0
JamesBremner <james@ravenspoint.com>
2024-11-16 07:01:19 -0500

    Display edge connections for each layer

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,153 @@
 #include <wex.h>
 #include "cStarterGUI.h"
 
+class cCell
+{
+public:
+    char myType;
+    cCell(char t)
+        : myType(t)
+    {
+    }
+    // Edge connection for specified layer
+    std::string connects(int layer) const;
+};
+class cGrid
+{
+public:
+    std::vector<std::vector<cCell>> myCells;
+    void addRow(const std::vector<char> &types);
+    int rowCount() const;
+    int colCount() const;
+    const cCell &cell(int row, int col) const;
+};
+
+cGrid gen1()
+{
+    cGrid ret;
+    ret.addRow({'L', '-', 'L'});
+    ret.addRow({'L', '-', 'L'});
+    ret.addRow({'-', '-', 'L'});
+    return ret;
+}
+
 class cGUI : public cStarterGUI
 {
 public:
     cGUI()
         : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+              "Rotating Cells",
+              {50, 50, 1000, 500})
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+
+        myGrid = gen1();
+
+        fm.events().draw(
+            [&](PAINTSTRUCT &ps)
+            {
+                wex::shapes S(ps);
+                draw(S);
+            });
 
         show();
         run();
     }
 
 private:
-    wex::label &lb;
+    cGrid myGrid;
+
+    // draw the edge connections for the layers
+    void draw(wex::shapes &S);
 };
+
+std::string cCell::connects(int layer) const
+{
+    switch (myType)
+    {
+    case 'L':
+        switch (layer)
+        {
+        case 0:
+            return "tr";
+        case 1:
+            return "rb";
+        case 2:
+            return "bl";
+        case 3:
+            return "lt";
+        default:
+            return "";
+        }
+
+    case '-':
+        switch (layer)
+        {
+        case 0:
+            return "lr";
+        case 1:
+            return "tb";
+        default:
+            return "";
+        }
+
+    default:
+        return "";
+    }
+}
+
+void cGrid::addRow(const std::vector<char> &types)
+{
+    if (myCells.size())
+    {
+        if (types.size() != myCells[0].size())
+            throw std::runtime_error(
+                "Bad column count");
+    }
+
+    std::vector<cCell> row;
+    for (char t : types)
+    {
+        row.emplace_back(t);
+    }
+    myCells.push_back(row);
+}
+int cGrid::rowCount() const
+{
+    return myCells.size();
+}
+int cGrid::colCount() const
+{
+    if (!myCells.size())
+        return 0;
+    return myCells[0].size();
+}
+const cCell &
+cGrid::cell(int row, int col) const
+{
+    return myCells[row][col];
+}
+
+void cGUI::draw(wex::shapes &S)
+{
+    const int colsize = 50;
+    const int rowsize = 20;
+    const int xoff = 20;
+    int yoff = - myGrid.rowCount() * rowsize;
+    for (int layer = 3; layer >= 0; layer--)
+    {
+        yoff += myGrid.rowCount() * rowsize;
+        S.text("Layer " + std::to_string(layer),
+               {0, yoff});
+        yoff += 20;
+
+        for (int row = 0; row < myGrid.rowCount(); row++)
+            for (int col = 0; col < myGrid.colCount(); col++)
+            {
+                S.text(myGrid.cell(row, col).connects(layer),
+                       {col * colsize + xoff, row * rowsize + yoff});
+            }
+    }
+}
 
 main()
 {
