26f98b046ad71fddac3dd7fbafe2cd0e3e29e3fb
JamesBremner <james@ravenspoint.com>
2024-11-16 09:23:13 -0500

    list graph edges

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ public:
     int rowCount() const;
     int colCount() const;
     const cCell &cell(int row, int col) const;
+    void graphEdges() const;
 };
 
 cGrid gen1()
@@ -48,6 +49,7 @@ public:
     {
 
         myGrid = gen1();
+        myGrid.graphEdges();
 
         fm.events().draw(
             [&](PAINTSTRUCT &ps)
@@ -83,7 +85,7 @@ std::string cCell::connects(int layer) const
         case 3:
             return "lt";
         default:
-            return "";
+            return "  ";
         }
 
     case '-':
@@ -94,11 +96,11 @@ std::string cCell::connects(int layer) const
         case 1:
             return "tb";
         default:
-            return "";
+            return "  ";
         }
 
     default:
-        return "";
+        return "  ";
     }
 }
 
@@ -131,7 +133,106 @@ int cGrid::colCount() const
 const cCell &
 cGrid::cell(int row, int col) const
 {
-    return myCells[row][col];
+    if (row >= 0 && row < rowCount() &&
+        col >= 0 && col < colCount())
+        return myCells[row][col];
+    static cCell null(' ');
+    return null;
+}
+
+void cGrid::graphEdges() const
+{
+    std::stringstream ss;
+
+    // cell internal connections
+    for (int layer = 3; layer >= 0; layer--)
+        for (int row = 0; row < rowCount(); row++)
+            for (int col = 0; col < colCount(); col++)
+            {
+                std::string sc = cell(row, col).connects(layer);
+                if (sc == "  ")
+                    continue;
+                ss << std::to_string(row)
+                   << "_" << std::to_string(col)
+                   << "_" << std::to_string(layer)
+                   << "_" << sc[0] << " "
+                   << std::to_string(row)
+                   << "_" << std::to_string(col)
+                   << "_" << std::to_string(layer)
+                   << "_" << sc[1] << " 0\n";
+            }
+
+    // layer internal connections
+    for (int layer = 3; layer >= 0; layer--)
+        for (int row = 0; row < rowCount(); row++)
+            for (int col = 0; col < colCount(); col++)
+            {
+                std::string srcl = std::to_string(row) +
+                                   "_" + std::to_string(col) +
+                                   "_" + std::to_string(layer);
+
+                int adj_row, adj_col;
+                std::string sc, sc_adj;
+
+                // check for connection with cell in same row, next column
+                adj_row = row;
+                adj_col = col + 1;
+                sc = cell(row, col).connects(layer);
+                sc_adj = cell(adj_row, adj_col).connects(layer);
+                if (sc_adj != "  ")
+                {
+                    if (sc[0] == 'r' || sc[1] == 'r')
+                    {
+                        if (sc_adj[0] == 'l' || sc_adj[1] == 'l')
+                            ss << srcl
+                               << "_r "
+                               << std::to_string(adj_row)
+                               << "_" << std::to_string(adj_col)
+                               << "_" << std::to_string(layer)
+                               << "_l 1\n";
+                    }
+                }
+
+                // check for connection with cell in next row, same column
+                adj_row = row + 1;
+                adj_col = col;
+                sc = cell(row, col).connects(layer);
+                sc_adj = cell(adj_row, adj_col).connects(layer);
+                if (sc_adj != "  ")
+                {
+                    if (sc[0] == 'b' || sc[1] == 'b')
+                    {
+                        if (sc_adj[0] == 't' || sc_adj[1] == 't')
+                            ss << srcl
+                               << "_b "
+                               << std::to_string(adj_row)
+                               << "_" << std::to_string(adj_col)
+                               << "_" << std::to_string(layer)
+                               << "_t 1\n";
+                    }
+                }
+            }
+
+    // connections between layers
+    for (int layer1 = 3; layer1 >= 1; layer1--)
+    {
+        int layer2 = layer1 - 1;
+        for (int row = 0; row < rowCount(); row++)
+            for (int col = 0; col < colCount(); col++) {
+                std::string srcl1 = std::to_string(row) +
+                                    "_" + std::to_string(col) +
+                                    "_" + std::to_string(layer1);
+                std::string srcl2 = std::to_string(row) +
+                                    "_" + std::to_string(col) +
+                                    "_" + std::to_string(layer2);
+                ss << srcl1 << "_t " << srcl2 << "_t 0\n";
+                ss << srcl1 << "_r " << srcl2 << "_r 0\n";
+                ss << srcl1 << "_b " << srcl2 << "_b 0\n";
+                ss << srcl1 << "_l " << srcl2 << "_l 0\n";
+            }
+    }
+
+    std::cout << ss.str();
 }
 
 void cGUI::draw(wex::shapes &S)
@@ -139,7 +240,7 @@ void cGUI::draw(wex::shapes &S)
     const int colsize = 50;
     const int rowsize = 20;
     const int xoff = 20;
-    int yoff = - myGrid.rowCount() * rowsize;
+    int yoff = -myGrid.rowCount() * rowsize;
     for (int layer = 3; layer >= 0; layer--)
     {
         yoff += myGrid.rowCount() * rowsize;
